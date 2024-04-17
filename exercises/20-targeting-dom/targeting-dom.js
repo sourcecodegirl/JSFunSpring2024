2c6c33405e19da44698a034f60e2b00ee9e98a73
sourcecodegirl <hello@sourcecodegirl.com>
2024-04-17 17:47:11 -0400

    Completed exercises 20 and 21

--- exercises/20-targeting-dom/targeting-dom.js
+++ exercises/20-targeting-dom/targeting-dom.js
@@ -12,4 +12,35 @@
  */
 (function () {
   // Put your answers in here
+
+  // first li element
+  const li = document.querySelector('li');
+
+  console.log(li);
+
+  // element with an id of myId
+  const myId = document.querySelector('#myId');
+
+  console.log(myId);
+
+  // every li element with a yellow background
+  const yellowBg = document.querySelectorAll('.bg-warning');
+  
+  console.log(yellowBg);
+
+  // button labeled target me
+  const targetMe = document.querySelector('#target');
+
+  console.log(targetMe);
+
+  // every a element that is social media
+  const social = document.querySelectorAll('[data-social-media]');
+  
+  console.log(social);
+
+  // target cell
+  const cell = document.querySelectorAll('#myRow div');
+
+  console.log(cell);
+
 })();
