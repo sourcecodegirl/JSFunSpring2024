402217a1db99547cac484e8becf7da240a3cab3f
sourcecodegirl <hello@sourcecodegirl.com>
2024-05-07 21:27:00 -0400

    Completed async-await exercise.

--- exercises/31-async-await/async-await.js
+++ exercises/31-async-await/async-await.js
@@ -8,4 +8,23 @@
    * This is the API you will be using. The method will be GET.
    * https://ron-swanson-quotes.herokuapp.com/v2/quotes
    */
+
+// get random quote from API and update quote
+const randomQuote = () => {
+  axios({
+    url: 'https://ron-swanson-quotes.herokuapp.com/v2/quotes',
+    method: 'GET',
+  })
+  .then(response => {
+    const quote = response.data;
+    const updateQuote = document.querySelector('#quote');
+    updateQuote.textContent = quote;
+  })
+  .catch((err) => console.error(err));
+};
+
+// listen for button click and update quote
+const randomQuoteBtn = document.querySelector('#button');
+randomQuoteBtn.addEventListener('click', randomQuote);
+
 })();
